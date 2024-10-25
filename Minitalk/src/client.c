4961f1ed56442030fbc787939f42dfb98fbc5d2c
Ruslan Khakimullin <ruslankhakimullin@gmail.com>
2024-10-25 13:06:58 +0300

    added client and server functions

--- Minitalk/src/client.c
+++ Minitalk/src/client.c
@@ -6,7 +6,34 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 17:29:46 by rkhakimu          #+#    #+#             */
-/*   Updated: 2024/10/24 17:29:47 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2024/10/25 13:04:59 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <signal.h>
+#include <unistd.h>
+#include <./lib/Libft/libft.h>
+#include <./lib/Libft/ft_printf.h>
+
+int	main(int ac, char **av)
+{
+	pid_t	server_pid;
+	char	*message;
+	
+	if (ac != 3)
+	{
+		write(2, "Usage: ./client <server_pid> <message>\n", 39);
+		return (1);
+	}
+	
+	server_pid = ft_atoi(av[1]);
+	message = av[2];
+	while(*message)
+	{
+		kill(server_pid, SIGUSR1);
+		usleep(100);
+		message++;
+	}
+	return (0);
+}
 
--- Minitalk/src/server.c
+++ Minitalk/src/server.c
@@ -6,7 +6,30 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 17:29:40 by rkhakimu          #+#    #+#             */
-/*   Updated: 2024/10/24 17:29:41 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2024/10/25 12:36:30 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <signal.h>
+#include <unistd.h>
+#include <./lib/Libft/libft.h>
+#include <./lib/Libft/ft_printf.h>
+
+void	signal_handler(int signum)
+{
+	if (signum == SIGUSR1)
+		write(1, "Received SIGUSR1 (0)\n", 21);
+	else if (signum == SIGUSR2)
+		write(1, "Received SIGUSR2 (1)\n", 21);
+}
+
+int	main(void)
+{
+	ft_printf("Server PID: %d\n", getpid());
+	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
+	while (1)
+		pause();
+	return (0);
+}
 
