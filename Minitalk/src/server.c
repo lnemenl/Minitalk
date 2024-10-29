a0e998b78bc75394dac16bef5a4fc7865f0a8d23
R <ruslankhakimullin@gmail.com>
2024-10-30 00:19:59 +0200

    replaced signal function with sigaction, added necessary stuct and modifyed signal_hadler function

--- Minitalk/src/server.c
+++ Minitalk/src/server.c
@@ -6,7 +6,7 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 17:29:40 by rkhakimu          #+#    #+#             */
-/*   Updated: 2024/10/28 20:22:27 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2024/10/30 00:12:06 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@
 
 void	handle_error(const char *error_message);
 
-void	signal_handler(int signum)
+void	signal_handler(int signum, siginfo_t *info, void *ucontext)
 {
 	static int	bit_count;
 	static char	current_char;
+	(void)info;
+	(void)ucontext;
 
 	if (signum == SIGUSR1)
 		current_char = (current_char << 1);
@@ -41,13 +43,19 @@ void	signal_handler(int signum)
 int	main(void)
 {
 	pid_t	pid;
+	struct	sigaction act;
 
 	pid = getpid();
 	if (pid <= 0)
 		handle_error("Error: Unable to receive PID");
 	ft_printf("Server PID: %d\n", pid);
-	signal(SIGUSR1, signal_handler);
-	signal(SIGUSR2, signal_handler);
+	sigemptyset(&act.sa_mask);
+	act.sa_sigaction = signal_handler;
+	act.sa_flags = SA_RESTART | SA_SIGINFO;
+	if (sigaction(SIGUSR1, &act, NULL) == -1)
+		handle_error("Error: Failed to set SIGUSR1 handler");
+	if (sigaction(SIGUSR2, &act, NULL) == -1)
+		handle_error("Error: Failed to set SIGUSR2 handler");
 	while (1)
 		pause();
 	return (0);
